66ad9402ec4eb64b5c46b1635f56894d0d7d8627
hui.wang09 <hui.wang09@bianlifeng.com>
2018-07-17 12:39:32 +0800

    guava-Strings

--- src/main/java/com/wanghui/guava/strings/StringsUse.java
+++ src/main/java/com/wanghui/guava/strings/StringsUse.java
@@ -1,9 +1,12 @@
 package com.wanghui.guava.strings;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 
 /**
  * 字符串工具类
@@ -20,10 +23,63 @@ public class StringsUse {
 	 * @since 2018/6/26
 	 */
 	public static void main(String[] args) {
+
+		/**
+		 * 判断字符是不是为空
+		 */
+		System.out.println(Strings.isNullOrEmpty(null));
+		/**
+		 * 如果字符为null转换成空串
+		 */
+		System.out.println(Strings.nullToEmpty(null));
+
+		/**
+		 * 获取两个字符串相同的前缀
+		 */
+		System.out.println(Strings.commonPrefix("bianlifeng.com", "bianlifeng.cn"));
+		/**
+		 * 获取两个字符串相同的后缀
+		 */
+		System.out.println(Strings.commonSuffix("wdes.vip.bianlifeng.com", "smp.corp.bianlifeng.com"));
+
+		/**
+		 * 补全字符串
+		 */
+		System.out.println(Strings.padEnd("Strings", 10, 'g'));
+		System.out.println(Strings.padStart("String", 10, 's'));
+
+		/**
+		 * 重复
+		 */
+		System.out.println(Strings.repeat("guava", 2));
+
+		/**
+		 * rimResults()方法去掉子串的空格，以及omitEmptyStrings()方法去掉空的子串
+		 * limit(int limit)方法，当分割的子字符串达到了limit个时，则停止分割
+		 */
+		Splitter splitter = Splitter.onPattern("[,]{1,}").trimResults().omitEmptyStrings();
+		System.out.println(splitter.split("hello,,,,,,world,java"));  //[hello, world，java]
+		System.out.println(splitter.limit(3).split("hello,,,,,,world,java,,,,spring"));	//[hello, world, java,,,,spring]
+
+		/**
+		 * Splitter类可以在结果中继续分割,直接返回Map的键值对
+		 */
+		System.out.println(splitter.withKeyValueSeparator("=").split("a=1,b=3,,c=4"));		//{a=1, b=3, c=4}
+
+		/**
+		 * joiner : 用分隔符把字符串序列连接起来，skipNulls()跳过空串
+		 */
 		Joiner joiner = Joiner.on(";").skipNulls();
 		System.out.println(joiner.join("Harry", null, "Ron", "Hermione"));
 		System.out.println(Joiner.on("-").join(Arrays.asList(1,5,7)));
 
-		Splitter splitter = Splitter.on(",");
+		/**
+		 * Joiner也可以将Map<String,String>做合并
+		 */
+		HashMap<String, String> hashMap = Maps.newHashMap();
+		hashMap.put("a","1");
+		hashMap.put("b", "23");
+		System.out.println(Joiner.on(";").withKeyValueSeparator("=").join(hashMap));
+
 	}
 }
